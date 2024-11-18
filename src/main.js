e0bd3c5842acebca31ef07e6c47dc7676c888cd9
Stéphane Ravet <ravet.st@gmail.com>
2024-11-18 13:01:23 +0100

    cache

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
 // src/main.js
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('Service Worker enregistré avec succès :', registration);
-    }).catch(error => {
-      console.log("Échec de l'enregistrement du Service Worker :", error);
-    });
-  });
-} else {
-  console.log('Le navigateur ne supporte pas les Service Workers.');
-}
+// Le service worker est géré automatiquement par vite-plugin-pwa
+// Vous pouvez supprimer le code d'enregistrement manuel du service worker
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,58 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  root: 'src',  // Définit 'src' comme répertoire racine
+  root: 'src',
   server: {
     watch: {
-      usePolling: true,  // S'assure que les changements sont surveillés même dans des environnements avec des systèmes de fichiers virtuels
+      usePolling: true,
     },
-    hmr: true,  // Active le Hot Module Replacement
-    open: true,  // Ouvre automatiquement le navigateur lors du démarrage du serveur
+    hmr: true,
+    open: true,
   },
   build: {
-    sourcemap: true,  // Génère des sourcemaps pour faciliter le débogage
+    sourcemap: true,
+    outDir: '../dist',
+    emptyOutDir: true,
   },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // Active le service worker en développement
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif,json}'],
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:js)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'js-cache',
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+            }
+          },
+          {
+            urlPattern: /\.(?:js|css)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources'
+            }
+          }
+        ]
+      },
+    })
+  ]
 });
